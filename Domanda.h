2a1de1f4ab11914261a08c060bf04258fdded72a
Endrit03 <gendrit32@gmail.com>
2021-05-02 18:14:56 +0200

    initial commit

---
+++ Domanda.h
@@ -0,0 +1,24 @@
+#ifndef DOMANDA_H
+#define DOMANDA_H
+
+#include<iostream>
+#include<cstdlib>
+#include<string>
+using namespace std;
+class Domanda{
+  private: 
+    string testo_domanda;
+    string risposte[4];
+    int risposta_corretta;
+     
+  public:  
+    Domanda();  
+    string getTestoDomanda();
+    void setDomanda(string s);
+    string getRisposta(int num_risposta);
+    void setRisposta(string s, int num_risposta);    
+    void setRispostaCorretta(int r);
+    int getRispostaCorretta();
+};
+
+#endif
